3c37fcdb395ab0e6dd6875ff7f90e2ca92aee6c6
gulpanjul <gulpa.andhikac@gmail.com>
2025-07-12 06:22:39 +0700

    feat: component and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+interface ButtonProps {
+  variant?: string;
+  children?: React.ReactNode;
+}
+
+const Button = (props: ButtonProps) => {
+  const { children, variant = "bg-black" } = props;
+  return (
+    <button
+      className={`h-10 px-6 font-semibold rounded-md ${variant}`}
+      type="submit"
+    >
+      {children}
+    </button>
+  );
+};
+
 function App() {
   return (
     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-      <div>Hello World</div>
+      <div className="flex gap-x-3">
+        <Button variant="bg-red-700">Login</Button>
+        <Button variant="bg-slate-700">Register</Button>
+        <Button></Button>
+      </div>
     </div>
   );
 }
